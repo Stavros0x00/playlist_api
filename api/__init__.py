b57ded2f31f0de918f43515ca587dc1db3ce47cb
Stavros0x00 <stavanto@gmail.com>
2017-12-30 18:56:31 +0200

    added app generation from a "factory" function for creating the opportunity to create flask blueprints and having better and appropriate configs when testing

--- api/__init__.py
+++ api/__init__.py
@@ -5,9 +5,22 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    return app
+
+
+app = create_app()
+
 
 from api import routes, models
--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,8 +1,10 @@
-# Pytest configuration file for this directory. Creates acousticbrainz and spotify
+# Pytest configuration file for this directory. Creates acousticbrainz, spotify and the whole app (api) flask object
 # objects used in the tests
 
 import pytest
+from api import create_app
 from api.auth import sp
+from config import TestingConfig
 from api.external import AcousticBrainz
 
 
@@ -19,3 +21,12 @@ def spotify_object():
     Setups the Spotify object.
     """
     return sp
+
+
+@pytest.fixture(scope='module')
+def api_object():
+    """
+    Setups the Flask app-api object.
+    """
+    app = create_app(config_class=TestingConfig)
+    return app
